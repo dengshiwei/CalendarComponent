fd64b83ea49479b66ceffea07b78634cf40fbdcc
dengshiwei <dengshiwei_it@163.com>
2016-08-10 00:01:29 +0800

    新增ADCircleMonthView类Theme主题

---
+++ library/src/main/java/com/dsw/calendar/theme/ADCircleDayTheme.java
@@ -0,0 +1,83 @@
+package com.dsw.calendar.theme;
+
+import android.graphics.Color;
+
+/**
+ * Created by Administrator on 2016/8/9.
+ */
+public class ADCircleDayTheme implements IDayTheme {
+    @Override
+    public int colorSelectBG() {
+        return Color.parseColor("#38C0C3");
+    }
+
+    @Override
+    public int colorSelectDay() {
+        return Color.parseColor("#FFFFFF");
+    }
+
+    @Override
+    public int colorToday() {
+        return Color.parseColor("#68CB00");
+    }
+
+    @Override
+    public int colorMonthView() {
+        return Color.parseColor("#FFFFFF");
+    }
+
+    @Override
+    public int colorWeekday() {
+        return Color.parseColor("#4F4F4F");
+    }
+
+    @Override
+    public int colorWeekend() {
+        return Color.parseColor("#BEBEBE");
+    }
+
+    @Override
+    public int colorDecor() {
+        return Color.parseColor("#4AB9AE");
+    }
+
+    @Override
+    public int colorRest() {
+        return Color.parseColor("#2AC5C8");
+    }
+
+    @Override
+    public int colorWork() {
+        return Color.parseColor("#C78D7D");
+    }
+
+    @Override
+    public int colorDesc() {
+        return Color.parseColor("#4F4F4F");
+    }
+
+    @Override
+    public int sizeDay() {
+        return 30;
+    }
+
+    @Override
+    public int sizeDesc() {
+        return 14;
+    }
+
+    @Override
+    public int sizeDecor() {
+        return 4;
+    }
+
+    @Override
+    public int dateHeight() {
+        return 70;
+    }
+
+    @Override
+    public int colorLine() {
+        return Color.parseColor("#CBCBCB");
+    }
+}
